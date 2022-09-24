908955bdc20267300649ddf0db099ddcf8b2c466
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-09-24 14:50:52 +0300

    completed assignment requirements

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, filterSelectedCategoryTasks}) {
+
+  function selectedButton(event) {
+    const siblings = [...event.target.parentElement.children];
+    siblings.map(sibling => sibling.className = "");
+    event.target.className = "selected";
+    filterSelectedCategoryTasks(event)
+  }
+
+  const listOfCategories = categories.map(category => (
+    <button key={category} onClick={selectedButton}>{category}</button>
+  ))
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {listOfCategories}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, deleteTask}) {
+
+  const myTasks = tasks.map((task, index) => (
+    <Task task={task} key={index} deleteTask={deleteTask} />
+  ))
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {myTasks}
     </div>
   );
 }
